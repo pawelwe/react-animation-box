6949f5434cb7a6253244c02e99bd9bae6876d482
Pawel <pawel@MacBook-Pro-Pawel.local>
2020-08-16 14:11:14 +0200

    adding disapearance test

--- src/components/AnimationTest/__tests__/AnimationTest.test.tsx
+++ src/components/AnimationTest/__tests__/AnimationTest.test.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 // @ts-ignore
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { AnimationTest } from '../AnimationTest';
 
 describe('Box component', () => {
+  it('should be visible after clicking show', async () => {
+    render(<AnimationTest />);
+    fireEvent.click(screen.getByText('Show Box 2'));
+    await waitFor(() => screen.getByText('BOX 2'));
+
+    expect(screen.getByText('BOX 2')).toBeInTheDocument();
+  });
+
+  it('should not be visible after clicking hide', async () => {
+    render(<AnimationTest />);
+    fireEvent.click(screen.getByText('Hide Box 1'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Show Box 1')).toBeInTheDocument();
+    });
+  });
+
   it('should have correct className', () => {
     const { container } = render(<AnimationTest />);
 
